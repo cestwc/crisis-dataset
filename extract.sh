80b5b86f55f092c45a25c8305394ef53bb282a2e
cestwc <80936226+cestwc@users.noreply.github.com>
2021-07-11 17:45:15 +0800

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -4,3 +4,4 @@ find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"
 rm -rf tweets-C26; mkdir tweets-C26; cp crisis-dataset/C26\ Dataset/Crisis\ Tweets/*-tweets_labeled.csv tweets-C26/; cp crisis-dataset/C26\ Dataset/*.csv  tweets-C26/
 rm -rf tweets-C6; mkdir tweets-C6; cp crisis-dataset/C6\ Dataset/Crisis\ Tweets\ C6/*.csv tweets-C6/; cp crisis-dataset/C6\ Dataset/*.csv  tweets-C6/
 rm -rf tweets-C8; mkdir tweets-C8; cp crisis-dataset/C8\ Dataset/*.csv tweets-C8/
+pip install -r requirements.txt
